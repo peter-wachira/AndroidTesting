1e5edce36e2e00e9717e28834a462db63552c6f9
peter-wachira <pwachira900@gmail.com>
2022-01-31 15:07:00 +0300

    Set up images adapter and inject in constructor of fragment

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ImagePickFragment.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ImagePickFragment.kt
@@ -4,14 +4,28 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.adapters.ImageAdapter
+import com.androiddevs.shoppinglisttestingyt.other.Constants.GRID_SPAN_COUNT
 import com.androiddevs.shoppinglisttestingyt.ui.viewmodel.ShoppingViewModel
+import kotlinx.android.synthetic.main.fragment_image_pick.*
+import javax.inject.Inject
 
-class ImagePickFragment: Fragment(R.layout.fragment_image_pick){
+class ImagePickFragment @Inject constructor(
+    private val imageAdapter: ImageAdapter
+): Fragment(R.layout.fragment_image_pick){
     lateinit var vieModel: ShoppingViewModel
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         vieModel = ViewModelProvider(requireActivity()).get(ShoppingViewModel::class.java)
+        setUpRecyclerView()
+    }
+
+    private fun setUpRecyclerView() {
+        rvImages.apply {
+            adapter = imageAdapter
+            layoutManager = GridLayoutManager(requireContext(),GRID_SPAN_COUNT)
+        }
     }
 }
